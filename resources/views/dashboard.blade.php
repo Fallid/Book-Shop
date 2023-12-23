cbfc04c7be5fb49e47953876bb2a046c7ceb9d55
zidan03 <noxiouszdn88@gmail.com>
2023-12-23 22:43:12 +0700

    feat: navbar and content

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="app.css"/>
+    <link rel="stylesheet" href="/css/app.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
     <title>Document</title>
     <link
@@ -14,10 +14,11 @@
     />
   </head>
   <body>
+    <header>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
       <div class="container-fluid">
         <a class="navbar-brand" href="#">
-          <img src="assets/bookhouse.png" width="80px" />
+          <img src="{{ asset('assets/bookhouse.png') }}" width="80px" />
         </a>
         <button
           class="navbar-toggler"
@@ -34,20 +35,41 @@
           >
           <form class="d-flex">
             <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn me-2 btn-outline-success" type="submit">Search</button>
-            <span><i class="bi bi-cart"></i></span>
+            <button class="btn btn-outline-success" type="submit">Search</button>
           </form>
         </div>
+        <span><i class="bi me-2 bi-cart"></i></span>
         <div class="d-grid gap-2 d-md-flex justify-content-md-end">
           <button class="btn btn-primary LOGIN" type="button">SIGN UP</button>
           <button class="btn btn-primary LOGIN" type="button">LOG IN</button>
         </div>
       </div>
+    </header>
+
+    <main>
+    <div class="home position-flex bottom-0 end-0" id="home">
+            <div class="home-text">
+                <h1>Koleksi Ebooks untuk <br> Pertumbuhan Pribadi dan <br> Profesional Anda with coffee</h1>
+                <p>Dalam e-book kami, temukan kisah nyata individu yang berhasil mencapai mimpi 
+                  dan tujuan mereka. Bukan sekadar motivasi, ini adalah rangkuman pengalaman hidup, 
+                  tantangan, dan strategi sukses.</p>
+            </div>
+            <aside class="home-img">
+              <img src="{{ asset('assets/bookcover.png') }}" alt="foto-main">
+            </aside>
+        </div>
+    </main>
+
+
       <script
         src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
         crossorigin="anonymous"
       ></script>
+
+
     </body>
+
+
     </html>
     
