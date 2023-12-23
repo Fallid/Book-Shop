e0b889e5241493d49df68c12522f6ab4e71c4988
Ahmad Naufal <91902493+Fallid@users.noreply.github.com>
2023-12-24 00:07:08 +0700

    Revert "feat: resize picture"

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -12,54 +12,6 @@
 
 <body>
     <header>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">
-          <div class="logo">
-            <img src="{{ asset('assets/bookhouse.png') }}" width="80px"/>
-          </div>
-        </a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-          ></button>
-          <div
-          class="collapse navbar-collapse justify-content-center"
-          id="navbarSupportedContent"
-          >
-          <form class="d-flex">
-            <input class="form-control me-2 col-2 rounded-pill" type="search" placeholder="Buku apa yang ingin dicari?" aria-label="Search"> 
-            <button class="btn btn-outline-success" type="submit">Search</button>
-          </form>
-        </div>
-        <span><i class="bi me-2 bi-cart"></i></span>
-        <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-          <button class="btn btn-primary LOGIN" type="button">SIGN UP</button>
-          <button class="btn btn-primary LOGIN" type="button">LOG IN</button>
-        </div>
-      </div>
-    </header>
-
-    <main>
-    <div class="home sm-5 text-start" id="home">
-            <div class="home-text">
-                <h1>Koleksi Ebooks untuk <br> Pertumbuhan Pribadi dan <br> Profesional Anda with coffee</h1>
-                <p>Dalam e-book kami, temukan kisah nyata individu yang berhasil <br> 
-                  mencapai mimpi dan tujuan mereka. Bukan sekadar motivasi, ini adalah <br>
-                  rangkuman pengalaman hidup, tantangan, dan strategi sukses.</p>
-            </div>
-            <aside class="home-img row">
-              <div class="col me-5">
-                <img src="{{ asset('assets/bookcover.png') }}" alt="foto-main">
-              </div>
-              <div class="col">
-                <img src="{{ asset('assets/bookcover1.jpg') }}" alt="foto-main1">
-              </div>
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
             <div class="container-fluid">
                 <a class="navbar-brand" href="#">
